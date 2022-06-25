702113dc3452e291b775bb3e2fd8b22d77277480
Arif Aydoğan <aydoganarif@yahoo.com>
2022-06-25 10:56:26 +0300

    Add project files.

---
+++ Project4-Algoritma/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Lütfen bir kelime ve bir numara giriniz: ");
+        string input = Console.ReadLine();
+        int indeks = int.Parse(input.Split(',')[0]);
+        string kelime = input.Split(",")[1];
+
+        if (indeks < kelime.Length)
+        {
+            string yeni = "";
+            for (int i = 0; i < kelime.Length; i++)
+            {
+                yeni = (i != indeks) ? (yeni + kelime[i]) : (yeni);
+            }
+            Console.WriteLine(yeni);
+        }
+        Console.WriteLine();
+    }
+    catch (Exception)
+    {
+
+    }
+}
